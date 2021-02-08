abca72e94b17b793cce7d69ea7dc66053a9f4460
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-02-08 23:46:12 +0100

    homepage: client check session cookie before request

--- static/js/home.js
+++ static/js/home.js
@@ -1,12 +1,18 @@
-$.ajax({
-    url: '/userdata',
-    method: 'GET'
-}).done(res => {
-    const username = JSON.parse(res);
-    if(!username){
-        document.body.innerHTML += `<a href='/login'><button>Login</button></a>`;
-    }
-    else{
-        document.body.innerHTML += `<a href='/desk'><button>${username}</button></a>`;
-    };
-});
+try{
+    const cookie = document.cookie.split('; ').find(row => row.startsWith('_taskID')).split('=')[1];
+    $.ajax({
+        url: '/userdata',
+        method: 'GET'
+    }).done(res => {
+        const username = JSON.parse(res);
+        if(!username){
+            document.body.innerHTML += `<a href='/login'><button>Login</button></a>`;
+        }
+        else{
+            document.body.innerHTML += `<a href='/desk'><button>${username}</button></a>`;
+        };
+    });
+}
+catch(err){
+    document.body.innerHTML += `<a href='/login'><button>Login</button></a>`;
+}
