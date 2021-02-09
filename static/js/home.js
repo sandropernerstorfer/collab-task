7bb81aa1bd7ba82cf67317a35150f9b67d7983f6
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-02-09 23:35:06 +0100

    homepage scroll events

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,10 @@
 const dynamicHeading = document.querySelector('#dynamic-heading');
 const dynamicRoute = document.querySelector('#dynamic-route');
+const aboutButton = document.querySelector('#about-button');
+const mainBox = document.querySelector('#main-content');
+const scrollTopButton = document.querySelector('#scroll-to-top');
 
-// Try to read sessionID Cookie -> call renderBannerContent function : parameter depends on cookie-status
+// Try to read sessionID Cookie -> call renderBannerContent(parameter depends on cookie-status)
 try{
     const cookie = document.cookie.split('; ').find(row => row.startsWith('_taskID')).split('=')[1];
     $.ajax({
@@ -37,4 +40,33 @@ function renderBannerContent(username){
     dynamicHeading.innerHTML = heading;
     dynamicRoute.innerHTML = `<button class="btn btn-outline-success button shadow-none">${html}</button>`;
     dynamicRoute.setAttribute('href',route);
+};
+
+// Smooth Scroll to Target
+aboutButton.addEventListener('click', e => {
+    let href = aboutButton.getAttribute("href");
+    let elem = document.getElementById(href.replace("#",""));
+    elem.scrollIntoView({ left: 0, block: 'start', behavior: 'smooth' });
+});
+
+// Scroll to Top of Document
+scrollTopButton.addEventListener('click', e => {
+    document.documentElement.scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+});
+
+// Observe if mainBox is in sight -> show and hide top-scroll-button
+let observer = new IntersectionObserver(callback);
+observer.observe(mainBox);
+function callback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            scrollTopButton.classList.add('show-btn')
+        }
+        else{
+            scrollTopButton.classList.remove('show-btn')
+        }
+    });
 };
