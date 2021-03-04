4ffe4c56d1cc524879cbaff223c435ba306664f0
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-03-05 00:15:44 +0100

    invite cards - dynamic styling

--- static/js/board.js
+++ static/js/board.js
@@ -416,3 +416,41 @@ sortForm.addEventListener('change', e => {
     renderDeskData();
     renderSharedData();
 });
+
+/**
+ * INVITE CARDS DYNAMIC COLORS
+ */
+document.querySelector('#invitesContainer').addEventListener('mouseover', e => {
+    if(!e.target.matches('.accept-button')) return;
+    const parent = e.target.closest('.invite-available');
+    const sibling = e.target.nextElementSibling;
+
+    parent.style.borderColor = '#23CE6B';
+    parent.style.color = '#23CE6B';
+    sibling.style.color = '#ccc';
+});
+document.querySelector('#invitesContainer').addEventListener('mouseover', e => {
+    if(!e.target.matches('.discard-button')) return;
+    const parent = e.target.closest('.invite-available');
+    const sibling = e.target.previousElementSibling;
+
+    parent.style.borderColor = 'rgba(220, 20, 60, 0.699)';
+    parent.style.color = 'rgba(220, 20, 60, 0.699)';
+    sibling.style.color = '#ccc';
+});
+document.querySelector('#invitesContainer').addEventListener('mouseout', e => {
+    if(e.target.matches('.accept-button') || e.target.matches('.discard-button')){
+        const parent = e.target.closest('.invite-available');
+        parent.style.borderColor = '#ccc';
+        parent.style.color = '#575757';
+        
+        if(e.target.matches('.accept-button')){
+            const sibling = e.target.nextElementSibling;
+            sibling.style.color = 'rgba(220, 20, 60, 0.699)';
+        }
+        else{
+            const sibling = e.target.previousElementSibling;
+            sibling.style.color = '#23CE6B';
+        }
+    }
+});
